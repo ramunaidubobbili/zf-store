c024b85bdb44477742333075d0672862d4ae70e5
Ramunaidu Bobbili <ramnaidu.bobbili@gmail.com>
2022-02-23 22:30:28 +0530

    Login validations

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -8,36 +8,69 @@ class Login extends React.Component {
 
     this.state = {
       islogged: false,
-      loginParams: {
-        user_id: "",
-        user_password: ""
-      }
+      user_id: '',
+      user_password: '',
+      userIdError: '',
+      userPasswordError: '',
     };
   }
 
-  handleFormChange = event => {
-    let loginParamsNew = { ...this.state.loginParams };
-    let val = event.target.value;
-    loginParamsNew[event.target.name] = val;
+  handleChange = (e) => {
+    const { name, value } = e.target
+    this.setState({ [name]: value })
+  }
+  
+  userNameValidation = (userid) => {
+    if(userid !== ""){
+      if(userid === "admin"){
+        this.setState({
+          userIdError: ""
+        })
+        return true
+      }
+      this.setState({
+        userIdError: "User id is incorrect."
+      })
+      return false
+    }
     this.setState({
-      loginParams: loginParamsNew
-    });
-  };
+      userIdError: "User id is required."
+    })
+    return false
+  }
+  passwordValidation = (pwd) => {
+    if(pwd !== ""){
+      if(pwd === "123"){
+        this.setState({
+          userPasswordError: ""
+        })
+        return true
+      }
+      this.setState({
+        userPasswordError: "User id is incorrect."
+      })
+      return false
+    }
+    this.setState({
+      userPasswordError: "User id is required."
+    })
+    return false
+  }
 
-  login = event => {
-    debugger;
-    let user_id = this.state.loginParams.user_id;
-    let user_password = this.state.loginParams.user_password;
-    if (user_id === "admin" && user_password === "123") {
+  login = (e) => {
+    let isValidUserId = this.userNameValidation(this.state.user_id);
+    let isValidPassword = this.passwordValidation(this.state.user_password);
+    if(isValidUserId && isValidPassword){
       localStorage.setItem("token", "T");
       this.setState({
         islogged: true
       });
     }
-    event.preventDefault();
+    (e).preventDefault();
   };
 
   render() {
+    const {userIdError, userPasswordError} = this.state;
     let token = localStorage.getItem("token");
     if (token) {
         return <Redirect to="/" />;
@@ -52,12 +85,14 @@ class Login extends React.Component {
                   <h5 className="mb-4 card-title">Login</h5>
                   <form noValidate="" autoComplete="off" className="form-inline">
                       <div className="mb-3 mr-sm-2 form-group">
-                          <label htmlFor="user_id" className="visually-hidden">Username</label>
-                          <input name="user_id" id="user_id" placeholder="User Name"  onChange={this.handleFormChange} className="py-2 px-3 form-control" />
+                        <label htmlFor="user_id" className="visually-hidden">Username</label>
+                        <input name="user_id" id="user_id" placeholder="User Name"  onChange={this.handleChange} className={"py-2 px-3 form-control " + (userIdError !== "" ? "is-invalid" : "")} />
+                        {userIdError !== "" && <div className='invalid-feedback text-start'>{userIdError}</div>}
                       </div>
                       <div className="mb-3 mr-sm-2 form-group">
-                          <label htmlFor="user_password" className="visually-hidden">Password</label>
-                          <input type="password" name="user_password" id="user_password" placeholder="Password" onChange={this.handleFormChange} className="py-2 px-3 form-control"/>
+                        <label htmlFor="user_password" className="visually-hidden">Password</label>
+                        <input type="password" name="user_password" id="user_password" placeholder="Password" onChange={this.handleChange} className={"py-2 px-3 form-control " + (userPasswordError !== "" ? "is-invalid" : "")}/>
+                        {userPasswordError !== "" && <div className='invalid-feedback text-start'>{userPasswordError}</div>}
                       </div>
                       <div className="d-grid gap-2">
                           <button type="submit" onClick={this.login} className="py-2 px-3 btn btn-primary" >Login</button>
